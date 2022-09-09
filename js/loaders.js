5b3bc14580e2d50a70e3572fea901511b39010bb
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2022-09-09 19:04:31 +0200

    adding folders and files

---
+++ js/loaders.js
@@ -0,0 +1,12 @@
+
+/*
+    Load common resources.
+    Depends on jQuery
+*/
+
+$(
+    function() {
+        $("#loadedFooter").load("footer.html");
+        $("#loadedHeader").load("header.html");
+    }
+);
