bad0a2477216122569bf9d3e6e4a2bab038f0520
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2022-09-20 12:29:33 +0200

    Transitions and loader
    Added fading image transition.
    readded footer loader

--- js/loaders.js
+++ js/loaders.js
@@ -3,10 +3,11 @@
     Load common resources.
     Depends on jQuery
 */
-
 $(
     function() {
+        console.log("Footer loader...");
+        
         $("#loadedFooter").load("footer.html");
-        $("#loadedHeader").load("header.html");
+        //$("#loadedHeader").load("header.html");
     }
 );
