fb79df4f6b7bed55041b1de4427e1053e59db51a
Gaute Lundal <gaute.lundal@gmail.com>
2022-12-10 12:47:08 +0100

    working form validator with 404 image

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,106 @@
 const form=document.querySelector("#contactForm");
-const firstName=document.querySelector("#fname");
-const firstNameError=document.querySelector("#formError");
-const email=document.querySelector("#email");
-const emailError=document.querySelector("#emailError");
+const inputname = document.querySelector("#inputname");
+const inputnameError = document.querySelector("#inputnameError");
+
+const mobile = document.querySelector("#mobile");
+const mobileError = document.querySelector("#mobileError");
+
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+
+const address = document.querySelector("#address");
+const addressError = document.querySelector("#addressError");
 
 
 function validateForm() {
-    event.preventDefault();
+    try {
+        event.preventDefault();
+        let status = true;
 
-    if(checkLenght(firstName.value,2)===true){
-        form.error.getElementsByClassName.display="none";
-    }else{
-        firstNameError.getElementsByClassName.display="block";
+        if(!validate(inputname,inputnameError,5)) status = false;
+        if(!validate(mobile,mobileError,8)) status = false;
+        if(!validate(address,addressError,10)) status = false;
+        if(!validate(email,emailError,10, true)) status = false;
+        
+        showValidated(status);
+    }
+    catch(error) {
+        createHtmlError(error);
     }
 }
 
-console.log("hello");
-   
-form.addEventListener("submit",validateForm);
+function showValidated(status) {
+    const formValidated = document.querySelector(".form-validated");
+    console.log("Validate status = " + status);
 
-function checkLenght(value,len) {
-    if(value.trim().lenght>len) {
-         return true;
+    if(status) {
+        formValidated.style.display = "block";
+        formValidated.innerHTML = "Message sent....";
     } else {
+        formValidated.style.display = "none";
+    }
+}
+
+function checkLength(input, length) {    
+    return (input.value.trim().length > length);
+}
+
+function validate(input,inputError,length,isEmail = false) {
+    if(input == null) {
+        console.log("validate input is null");
         return false;
     }
+    if(inputError == null) {
+        console.log("validate inputError is null");
+        return false;
+    }
+
+    if(isEmail) {
+        if(isEmailValid(input.value) === true) {
+            inputError.style.display = "none";
+            return true;
+        } else {
+            inputError.style.display = "block";
+            return false;            
+        }
+    } else {
+        if(checkLength(input,length) === true) {
+            inputError.style.display = "none";
+            return true;
+        } else {
+            inputError.style.display = "block";
+            return false;
+        }
+    }   
+}
+
+
+function checkLength(input, length) {    
+    return (input.value.trim().length >= length);
 }
      
-function validateEmail(email){
-    const regEx=/\S+@\S+\.\S+/;
-    const patternMatches=regEx.test(email);
-    return patternMatches;
+function isEmailValid(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    return regEx.test(email);
 }
     
+
+function createHtmlError(error) {
+    const container = document.querySelector(".boks-d");
+    console.log("Exception: " + error);
+    document.body.style.backgroundColor = "white";
+    
+    container.innerHTML = `<div class="title"><h1>OPS 404 ERROR.....</h1></div>
+                            <img src="/images/404.webp" height="200" style="max-width: 240px">
+                            <div class="details-date">An error occurred processing data</div>
+                            
+                            <div class="home"><a href="pamelding.html"><h1>Return</h1></a></div>
+                            `;
+    
+    const title = document.querySelector(".title");
+    title.style.color = "black";
+}
+
+console.log("Form validator loaded...");   
+form.addEventListener("submit",validateForm);
